9163a0a94e8eb6c71982826bc18de5b9f02a0ac6
yaboyanees <a.merzi@yahoo.com>
2015-07-26 20:29:04 -0400

    s3 amazon update

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: "AKIAIZTQDKLZHER4HO5A",
-  secret_access_key: "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",
-  region: "us-east-1",
-  bucket: "mychu",
+  access_key_id: "ACCESS ID HERE",
+  secret_access_key: "ACCESS KEY HERE",
+  region: "REGION HERE",
+  bucket: "NAME HERE",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
