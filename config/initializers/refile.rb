25b46d0554631acf35391a6db371041f90e8a246
yaboyanees <a.merzi@yahoo.com>
2015-09-20 11:00:48 -0400

    fixed some dependency issues in gemfile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: "AKIAJJY2PNI2QLWT2WRA",
-  secret_access_key: "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",
-  region: "us-east-1",
-  bucket: "mychu",
+  access_key_id: "KEY ID",
+  secret_access_key: "ACCESS_KEY",
+  region: "REGION",
+  bucket: "NAME",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
